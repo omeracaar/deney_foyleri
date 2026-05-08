3766f8a9224b6e1aae9c26d133d12fb2a30bce20
Ömer Acar <omeracar34@icloud.com>
2026-05-08 12:17:37 +0300

    fix

--- src/data/experiments.js
+++ src/data/experiments.js
@@ -1,3 +1,5 @@
+const BASE_URL = import.meta.env.BASE_URL;
+
 export const experiments = [
   {
     id: "zil-devresi",
@@ -7,18 +9,18 @@ export const experiments = [
       {
         id: "power",
         name: "Güç Kaynağı",
-        image: "/images/power.png",
+        image: `${BASE_URL}images/power.png`,
       },
       {
         id: "button",
         name: "Buton",
-        image: "/images/button.png",
+        image: `${BASE_URL}images/button.png`,
       },
       {
         id: "bell",
         name: "Zil",
-        image: "/images/bell.png",
-        sound: "/sounds/bell.mp3",
+        image: `${BASE_URL}images/bell.png`,
+        sound: `${BASE_URL}sounds/bell.mp3`,
       },
     ],
     slots: [
@@ -48,52 +50,52 @@ export const experiments = [
       {
         id: "power",
         name: "Güç Kaynağı",
-        image: "/images/power.png",
+        image: `${BASE_URL}images/power.png`,
       },
       {
         id: "fuse",
         name: "Sigorta",
-        image: "/images/fuse.png",
+        image: `${BASE_URL}images/fuse.png`,
       },
       {
         id: "meter",
         name: "Elektrik Sayacı",
-        image: "/images/meter.png",
+        image: `${BASE_URL}images/meter.png`,
       },
       {
         id: "dimmer",
         name: "Dimmer Anahtar",
-        image: "/images/dimmer.png",
+        image: `${BASE_URL}images/dimmer.png`,
       },
       {
         id: "lamp",
         name: "Lamba",
-        image: "/images/lamp-off.png",
-        activeImage: "/images/lamp-on.png",
+        image: `${BASE_URL}images/lamp-off.png`,
+        activeImage: `${BASE_URL}images/lamp-on.png`,
       },
     ],
     slots: [
-  {
-    id: "slot-1",
-    label: "1. Boşluk",
-  },
-  {
-    id: "slot-2",
-    label: "2. Boşluk",
-  },
-  {
-    id: "slot-3",
-    label: "3. Boşluk",
-  },
-  {
-    id: "slot-4",
-    label: "4. Boşluk",
-  },
-  {
-    id: "slot-5",
-    label: "5. Boşluk",
-  },
-],
+      {
+        id: "slot-1",
+        label: "1. Boşluk",
+      },
+      {
+        id: "slot-2",
+        label: "2. Boşluk",
+      },
+      {
+        id: "slot-3",
+        label: "3. Boşluk",
+      },
+      {
+        id: "slot-4",
+        label: "4. Boşluk",
+      },
+      {
+        id: "slot-5",
+        label: "5. Boşluk",
+      },
+    ],
     correctOrder: ["power", "fuse", "meter", "dimmer", "lamp"],
     successAction: "lamp",
   },
